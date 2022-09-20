e10193f9e12b27ce1386456a14c8d6826453e786
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-09-20 09:17:55 +0100

    algoritm question solved.

--- script.js
+++ script.js
@@ -211,45 +211,83 @@
 
 
 // ** Array methods **
-const friends = ['AbuCho', 'ChoBoy', "AbuMrOla", 'AbuTahuZee'];
+// const friends = ['AbuCho', 'ChoBoy', "AbuMrOla", 'AbuTahuZee'];
 // will add to the end of an array.
-friends.push('AbuZulu');
-console.log(friends);
+// friends.push('AbuZulu');
+// console.log(friends);
 
 
 // Add element to the beginning of an array
-friends.unshift('AbuFilm');
-console.log(friends);
+// friends.unshift('AbuFilm');
+// console.log(friends);
 
 // Remove last element from the array.
-friends.pop()
-console.log(friends);
-const removedElement = friends.pop();
-console.log(removedElement);
-console.log(friends);
+// friends.pop()
+// console.log(friends);
+// const removedElement = friends.pop();
+// console.log(removedElement);
+// console.log(friends);
 
 // Remove the first element from the array.
-friends.shift();
-console.log(friends);
+// friends.shift();
+// console.log(friends);
 
 
 // Get a partucular position of a particular element in an array.
-const getIndex = friends.indexOf('ChoBoy')
-console.log(getIndex);
+// const getIndex = friends.indexOf('ChoBoy')
+// console.log(getIndex);
 // if you do that for the element that is not there you will get [-1].
 
 // Check wether the element is in the array or not.
-console.log(friends.includes('AbuFilm'));
-console.log(friends.includes('ChoBoy'));
+// console.log(friends.includes('AbuFilm'));
+// console.log(friends.includes('ChoBoy'));
 // it also checks for equality types [ string or number]
-friends.push(80);
+// friends.push(80);
 // console.log(friends.includes('80'));
-console.log(friends.includes(80));
+// console.log(friends.includes(80));
 
 
 // The includes can be use to wriet conditionals
-const singleFriend = 'ChoBoy'
+// const singleFriend = 'ChoBoy'
 
-if(friends.includes(singleFriend)) {
-    console.log(`You have a friend  called "${singleFriend}" `);
+// if(friends.includes(singleFriend)) {
+//     console.log(`You have a friend  called "${singleFriend}" `);
+// }
+
+// ** Coding challenge.
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+// 20%.
+// Your tasks:
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
+// type you like the most. Test the function using a bill value of 100
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
+// below
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
+// the function you created before
+// 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+// Test data: 125, 555 and 44
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array) 
+// GOOD LUCK 
+
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
 }
+
+// Arrow function method
+// const calcTip = bill =>  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+
+
+const bills  = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(total);
